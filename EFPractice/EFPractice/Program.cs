b2a25ab0a18724048658105ec0477afd66b0e326
Sri Vardhana Korada <korada.srivardhana@publicissapient.com>
2020-10-02 23:49:57 +0530

    Adding Codebase

---
+++ EFPractice/EFPractice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using NLog;
+namespace EFPractice
+{
+    class Program
+    {
+        protected Program()
+        {
+
+        }
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        public static void Main(string[] args)
+        {
+            using (var dbContext = new EmployeeDatabaseEntities())
+            {
+                logger.Info("Starting looping the departments");
+                var departmentsList = dbContext.Departments;
+                foreach (var department in departmentsList)
+                {
+                    logger.Info($"Operatin on department {department.Name} at location {department.Location}");
+                    var employeesList = department.Employees;
+                    foreach (var employee in employeesList)
+                    {
+                        logger.Info($"Employee : Name = {employee.Name}, Email = {employee.Email}, Salary = {employee.Salary}");
+                    }
+                }
+
+                var itEmployees = dbContext.Employees.AsQueryable().Where(emp => emp.Department.Name.Equals("IT"));
+                foreach (var emp in itEmployees)
+                {
+                    logger.Info($"Employee in IT Depratment : {emp.Name}, {emp.Salary}");
+                }
+            }
+        }
+    }
+}
---
+++ JsonPractice/JsonPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+namespace JsonPractice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var employee = new Employee
+            {
+                Age = 10,
+                EmployeeId = 1,
+                EmployeeName = "Vardhan"
+            };
+            var serializedString = JsonConvert.SerializeObject(employee);
+            WriteToFile(serializedString, @"C:\Bootcamp\OwnPractice\BootCampPractice\JsonPractice\employeeJson.json");
+
+        }
+
+        private static void WriteToFile(string serializedString, string path)
+        {
+            using var file = new StreamWriter(path);
+            file.WriteLine(serializedString);
+        }
+    }
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public int Age { get; set; }
+    }
+}
